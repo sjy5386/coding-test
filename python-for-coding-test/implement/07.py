44d934092a6f02453d274c8fc3ab7000e29f3903
Seokjin Yoon <sjy5386@gmail.com>
2022-04-29 18:31:10 +0900

    Add python for coding test implement 07

---
+++ python-for-coding-test/implement/07.py
@@ -0,0 +1,21 @@
+"""
+럭키 스트레이트
+"""
+
+
+def solution(n: int) -> str:
+    arr = list(map(int, list(str(n))))
+    center = len(arr) // 2
+    left = sum(arr[:center])
+    right = sum(arr[center:])
+    return 'LUCKY' if left == right else 'READY'
+
+
+def main():
+    n = int(input())
+    print(solution(n))
+
+
+if __name__ == '__main__':
+    print(solution(123402))
+    print(solution(7755))
