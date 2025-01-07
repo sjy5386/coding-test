8efd8995d088540221adae9441a915471db3d8a7
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-01-07 22:34:09 +0900

    Add programmers 181951
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

---
+++ programmers/level0/181951.py
@@ -0,0 +1,15 @@
+"""
+a와 b 출력하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181951
+"""
+
+
+def solution(a, b):
+    print(f'a = {a}')
+    print(f'b = {b}')
+
+
+if __name__ == '__main__':
+    solution(4, 5)
+    # a = 4
+    # b = 5
