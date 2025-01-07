9ed94bb42d187dfbd4db7b6192504d0679065396
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-01-07 22:31:02 +0900

    Add programmers 181952
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

---
+++ programmers/level0/181952.py
@@ -0,0 +1,12 @@
+"""
+문자열 출력하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181952
+"""
+
+
+def solution(str):
+    print(str)
+
+
+if __name__ == '__main__':
+    solution('HelloWorld!')  # HelloWorld!
