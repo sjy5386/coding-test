ec4da8b9f73d2db2ee7437c1d2c7ccd3d2cf8840
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-02-06 00:06:15 +0900

    Add programmers 181848
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

---
+++ programmers/level0/181848.py
@@ -0,0 +1,13 @@
+"""
+문자열을 정수로 변환하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181848
+"""
+
+
+def solution(n_str):
+    return int(n_str)
+
+
+if __name__ == '__main__':
+    print(solution('10'))  # 10
+    print(solution('8542'))  # 8542
