75106b6cbed2ced56fe1b0f4d56c0471463512c3
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-02-08 22:03:18 +0900

    Add programmers 120810
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

---
+++ programmers/level0/120810.py
@@ -0,0 +1,13 @@
+"""
+나머지 구하기
+https://school.programmers.co.kr/learn/courses/30/lessons/120810
+"""
+
+
+def solution(num1, num2):
+    return num1 % num2
+
+
+if __name__ == '__main__':
+    print(solution(3, 2))  # 1
+    print(solution(10, 5))  # 0
